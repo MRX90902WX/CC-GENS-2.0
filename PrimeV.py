c82af4fc9cea01835e1485160c3d72e736290a35
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-06-05 14:16:46 -0500

    Update PrimeV.py

--- PrimeV.py
+++ PrimeV.py
@@ -24,6 +24,7 @@ print("")
 print(" #1. BIN AUSTRALIA")
 print(" #2. BIN CANADA")
 print(" #3. BIN ALBANIA")
+print(" #4. BIN MEXICO")
 print("") 
 main = input(" \033[1;32m----> ")
 print("")
@@ -34,6 +35,8 @@ elif main == "2":
    system("python chk2-ca.py")
 elif main == "3":
    system("python chk3-al.py")
+elif main == "4":
+   system("python chk4-mx.py")    
 else:
    print("\033[1;34m[X]\033[1;31mOPCIÓN INCORRECTA")
 
